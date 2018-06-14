b54fb836af956128400a86564de5f0c3883e2d32
nisen <nis@16care.com>
2018-06-14 15:07:33 +0800

    Fix an error of trying to (accept socket) in new thread, after (wait-for-input socket) in main thread, so that itmakes lots of useless threads.

--- src/weaver.lisp
+++ src/weaver.lisp
@@ -6,38 +6,52 @@
 
 ;; output content from client
 ;; plus some mark then return to client
-(defun handle-connection (socket)
+(defun handle-connection (connection)
   (handler-case
-      (let* ((connection (usocket:socket-accept socket))
-             (stream (usocket:socket-stream connection)))
+      (let* ((stream (usocket:socket-stream connection)))
         (handle-request stream)
+
         (log:info "Connection ends")
-        ;; (usocket:socket-close connection) ;;need it?
+
         (close stream)
+        (usocket:socket-close connection)
         )
     (error (e)
       (log:error e))))
 
-(defun accept-connection (socket)
-  (handler-case (bt:make-thread
-                 (lambda () (handle-connection socket)))
-    (error (e)
-      (log:error e)
-      (error e))))
+(defun accept-socket (socket)
+
+  (let ((connection (handler-case
+                        (usocket:socket-accept socket)
+                      ;; ignore condition here?
+                      (usocket:connection-aborted-error ()))))
+    (handler-case (bt:make-thread
+                   (lambda () (handle-connection connection)))
+      (error (e)
+        (usocket:socket-close connection) ;; 考虑这个是否妥当
+        (log:error "Error while creating worker thread for new incoming connection")
+        ))))
 
 
-;; (defvar *keep-going* t)
-(defvar *running-socket* nil)
+(defparameter *keep-going* t)
+;; (defvar *running-socket* nil)
+;; (defvar *host* nil)
+;; (defvar *port* nil)
 
 (defun serve (host port)
   (handler-case
       (usocket:with-socket-listener
           (socket host port :reuse-address t :element-type '(unsigned-byte 8))
         (log:info "Listening for connections...")
-        (setf *running-socket* socket)
-        (loop while (usocket:wait-for-input socket :ready-only t)
-           do (accept-connection socket))
-        (log:info "Server stop.")
+
+        ;; (setf *running-socket* socket)
+        (loop
+           (if (not *keep-going*) (return))
+
+           (when (usocket:wait-for-input socket :ready-only t :timeout 2)
+             (accept-socket socket)))
+
+        (log:info "Server stop...")
         )
     (error (e)
       (log:error e))))
@@ -53,5 +67,8 @@
 
 ;; TODO fix it , now close rudely
 (defun stop ()
-  ;; (setf *keep-going* nil)
-  (usocket:socket-close *running-socket*))
+  (setf *keep-going* nil)
+  (sleep 1)
+  t
+  ;; (usocket:socket-close *running-socket*)
+  )
